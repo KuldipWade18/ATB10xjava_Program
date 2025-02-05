a4db4ba96827f47f1063a200d36aa1acde1dbba0
Kuldip wade <wadekuldip@gmail.com>
2025-02-05 22:56:24 +0530

    5 Feb

---
+++ src/Java_Basic/Lab07_Table_of_7.java
@@ -0,0 +1,22 @@
+package Java_Basic;
+
+public class Lab07_Table_of_7
+{
+    public static void main(String[] args) {
+        int num=7;
+        System.out.printf("%d*1=%d\n,",num,num);
+        System.out.printf("%d*2=%d\n,",num,num*2);
+        System.out.printf("%d*3=%d\n,",num,num*3);
+        System.out.printf("%d*4=%d\n,",num,num*4);
+        System.out.printf("%d*5=%d\n,",num,num*5);
+        System.out.printf("%d*6=%d\n,",num,num*6);
+        System.out.printf("%d*7=%d\n,",num,num*7);
+        System.out.printf("%d*8=%d\n,",num,num*8);
+        System.out.printf("%d*9=%d\n,",num,num*9);
+        System.out.printf("%d*10=%d\n,",num,num*10);
+
+
+
+
+    }
+}
