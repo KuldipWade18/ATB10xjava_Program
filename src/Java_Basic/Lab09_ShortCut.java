e5cd96b71ddd4642685a44945895e6b11393fe73
Kuldip wade <wadekuldip@gmail.com>
2025-02-07 15:44:18 +0530

    7 Feb_1

--- src/Java_Basic/Lab09_ShortCut.java
+++ src/Java_Basic/Lab09_ShortCut.java
@@ -1,9 +1,14 @@
 package Java_Basic;
 
+import java.sql.SQLOutput;
+
 public class Lab09_ShortCut
 {
     public static void main(String[] args) {
         System.out.println("Hi ");//main sout
+
+        int a=10;
+        System.out.println(a);
 
 
     }
